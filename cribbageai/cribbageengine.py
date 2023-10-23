53ff424c0fb825641fb9036bc27ead2ec8d6ec6e
Jordan Reed <jreed@extole.com>
2023-10-23 12:54:30 -0700

    Fixed main pylinting issues with basic formatting and documentation. Still need a few code restructure issues

--- cribbageai/cribbageengine.py
+++ cribbageai/cribbageengine.py
@@ -58,13 +58,13 @@ class PlayingCard:
         """Gets the unicode character for the suite for display purposes."""
         suit_display = ""
         if self.suit == Suit.CLUB:
-          suit_display = "♣"
+            suit_display = "♣"
         elif self.suit == Suit.DIAMOND:
-          suit_display = "♦"
+            suit_display = "♦"
         elif self.suit == Suit.SPADE:
-          suit_display = "♠"
+            suit_display = "♠"
         elif self.suit == Suit.HEART:
-          suit_display = "♥"
+            suit_display = "♥"
 
         return suit_display
 
@@ -72,37 +72,37 @@ class PlayingCard:
         """Gets a character for the face value for display purposes."""
         face_display = ""
         if self.face == Face.ACE:
-          face_display = "A"
+            face_display = "A"
         if self.face == Face.TWO:
-          face_display = "2"
+            face_display = "2"
         if self.face == Face.THREE:
-          face_display = "3"
+            face_display = "3"
         if self.face == Face.FOUR:
-          face_display = "4"
+            face_display = "4"
         if self.face == Face.FIVE:
-          face_display = "5"
+            face_display = "5"
         if self.face == Face.SIX:
-          face_display = "6"
+            face_display = "6"
         if self.face == Face.SEVEN:
-          face_display = "7"
+            face_display = "7"
         if self.face == Face.EIGHT:
-          face_display = "8"
+            face_display = "8"
         if self.face == Face.NINE:
-          face_display = "9"
+            face_display = "9"
         if self.face == Face.TEN:
-          face_display = "10"
+            face_display = "10"
         if self.face == Face.JACK:
-          face_display = "J"
+            face_display = "J"
         if self.face == Face.QUEEN:
-          face_display = "Q"
+            face_display = "Q"
         if self.face == Face.KING:
-          face_display = "K"
+            face_display = "K"
 
         return face_display
 
     def get_display(self):
         """Gets a display that combines the face value and the suit."""
-        return "%s%s" % (self.get_face_display(),self.get_suit_display())
+        return f"{self.get_face_display()}{self.get_suit_display()}"
 
 
     def __eq__(self, other):
@@ -119,17 +119,24 @@ class PlayingCard:
 
         if self.face.value < other.face.value:
             return True
-        elif self.face.value > other.face.value:
+
+        if self.face.value > other.face.value:
             return False
-        elif self.suit.value < other.suit.value:
+
+        if self.suit.value < other.suit.value:
             return True
-        else:
-            return False
+
+        return False
 
     def __hash__(self):
         return hash(str(self.suit) + str(self.face))
 
 class CribbageGame:
+    """Holds the state information for a game of cribbage.
+
+    Attributes:
+        game_deck: A set of PlayingCards still in the deck for the game.
+    """
     def __init__(self, base_deck, player_one, player_two):
         self._base_deck = base_deck
         self.game_deck = set(self._base_deck)
@@ -186,43 +193,59 @@ class CribbageGame:
             self.player_two_hand.add(card)
 
         logging.info("Hands are dealt --")
-        logging.info("P1 Hand: %s" % (cards_as_string(self.player_one_hand)))
-        logging.info("P2 Hand: %s" % (cards_as_string(self.player_two_hand)))
+        logging.info("P1 Hand: %s", cards_as_string(self.player_one_hand))
+        logging.info("P2 Hand: %s", cards_as_string(self.player_two_hand))
 
     def discard_to_crib(self):
+        """Allows both players to pick two cards to put into the crib."""
+        # TODO - should pass immutable hands to players and return
+        # cards.  Let the engine remove from hand and put in crib.
         self.player_one.discard_to_crib(self.player_one_hand, self.crib)
         self.player_two.discard_to_crib(self.player_two_hand, self.crib)
         logging.info("Discarded to Crib --")
-        logging.info("P1 Hand: %s" % (cards_as_string(self.player_one_hand)))
-        logging.info("P2 Hand: %s" % (cards_as_string(self.player_two_hand)))
-        logging.info("Crib: %s" % (cards_as_string(self.crib)))
+        logging.info("P1 Hand: %s", cards_as_string(self.player_one_hand))
+        logging.info("P2 Hand: %s", cards_as_string(self.player_two_hand))
+        logging.info("Crib: %s", cards_as_string(self.crib))
 
     def cut_start_card(self):
+        """Picks a random start card and check for his heels (2 points to dealer)"""
         card = random.sample(sorted(self.game_deck), 1)[0]
         self.game_deck.remove(card)
         self.start_card.add(card)
-        logging.info("Start Card: %s" % (card.get_display()))
+        logging.info("Start Card: %s", card.get_display())
 
         ## If it's a jack, dealer gets 2 points
         if card.face == Face.JACK:
             if self.run_turn == 2:
                 self.player_one_score += 1
-                logging.info("Dealer Gets His Heels for +2: %s" % (self.player_one_score))
+                logging.info("Dealer Gets His Heels for +2: %s", self.player_one_score)
             else:
                 self.player_two_score += 1
-                logging.info("Dealer Gets His Heels for +2: %s" % (self.player_two_score))
+                logging.info("Dealer Gets His Heels for +2: %s", self.player_two_score)
 
         self.player_one_run_hand = set(self.player_one_hand)
         self.player_two_run_hand = set(self.player_two_hand)
 
     def is_more_run_cards(self):
+        """Checks if either player has more cards in their run hand to play.
+        Returns:
+          (boolean): If there are cards left to play
+        """
         if not self.player_one_run_hand and not self.player_two_run_hand:
             return False
-        else:
-            return True
+
+        return True
 
     def play_next_run_card(self):
+        """Plays the next run card on the current game.
+
+        This method will execute the next run card and update the state of the game.
+        If there is no way to play a run card, then it will exit.
+        """
         if self.is_more_run_cards():
+            # Set the following method variables for the run
+            # active_run_player - the AI for the active player
+            # active_run_hand - the active player's hand
             if self.run_turn == 1:
                 active_run_player = self.player_one
                 active_run_hand = self.player_one_run_hand
@@ -231,69 +254,92 @@ class CribbageGame:
                 active_run_hand = self.player_two_run_hand
 
             ## Test if the player can play a card and stay under 31
-            run_total = CribbageGame.get_cards_total_value(self.run);
-            logging.info("Checking if player #%i can play against run %i"
-              % (self.run_turn, run_total))
+            run_total = CribbageGame.get_cards_total_value(self.run)
+            logging.info("Checking if player #%i can play against run %i",
+              self.run_turn, run_total)
 
-            can_play_card = False
-            for card in active_run_hand:
-                logging.debug("Player #%i run total [%i] + card %s [%i]"
-                  % (self.run_turn, run_total, card.get_display(), card.value))
-
-                if run_total + card.value <= HIGHEST_RUN_ALLOWED:
-                    can_play_card = True
-
-            if can_play_card:
-                logging.info("Player #%i can play against run %i" % (self.run_turn, run_total))
-                run_card = active_run_player.get_run_card(
-                             active_run_hand, self.run, run_total)
-
-                points_for_card = calculate_score_for_run_play(self.run, run_card)
-                logging.info("Player #%i played %s for %i points against the run %s"
-                  % (self.run_turn, run_card.get_display(), points_for_card,
-                     cards_as_string(self.run)))
-
-
-                active_run_hand.remove(run_card)
-                self.run.append(run_card)
-
-                logging.info("Player #%i played %s, the run is now %s"
-                  % (self.run_turn, run_card.get_display(), cards_as_string(self.run)))
+            if self._can_play_card(active_run_hand):
+                self._play_run_card(active_run_player, active_run_hand)
 
             else:
-                logging.info("Player #%i cannot play against run %i" % (self.run_turn, run_total))
+                logging.info("Player #%i cannot play against run %i",
+                  self.run_turn, run_total)
+
                 # The active player cannot play.
                 # If no one has said "Go" - the active player says "Go"
                 if self.go_player == 0:
                     self.go_player = self.run_turn
-                    logging.info("Player #%i call a Go" % (self.run_turn))
+                    logging.info("Player #%i call a Go", self.run_turn)
                     # The other player gets one point when Go happens.
                     if self.run_turn == 1:
-                      self.player_two_score += 1
-                      logging.info("Player #2 scores to total %i" % (self.player_two_score))
+                        self.player_two_score += 1
+                        logging.info("Player #2 scores to total %i", self.player_two_score)
                     else:
-                      self.player_one_score += 1
-                      logging.info("Player #1 scores to total %i" % (self.player_one_score))
+                        self.player_one_score += 1
+                        logging.info("Player #1 scores to total %i", self.player_one_score)
 
                 # If someone has said "Go" and it was the other player
                 # then reset the run
                 elif self.go_player != self.run_turn:
-                	self.run = []
-                	self.go_player = 0
+                    self.run = []
+                    self.go_player = 0
 
             # The last card gets one more point
             if not self.is_more_run_cards():
                 if self.run_turn == 1:
-                  self.player_one_score += 1
-                  logging.info("Player #1 plays last card scores to total %i"
-                    % (self.player_one_score))
+                    self.player_one_score += 1
+                    logging.info("Player #1 plays last card scores to total %i",
+                      (self.player_one_score))
                 else:
-                  self.player_two_score += 1
-                  logging.info("Player #2 plays last card scores to total %i"
-                    % (self.player_two_score))
+                    self.player_two_score += 1
+                    logging.info("Player #2 plays last card scores to total %i",
+                      (self.player_two_score))
 
 
             self.run_turn = 2 if self.run_turn == 1 else 1
+
+    def _can_play_card(self, active_run_hand):
+        can_play_card = False
+        run_total = CribbageGame.get_cards_total_value(self.run)
+        logging.info("Checking if player #%i can play against run %i",
+          self.run_turn, run_total)
+
+        for card in active_run_hand:
+            logging.debug("Player #%i run total [%i] + card %s [%i]",
+              self.run_turn, run_total, card.get_display(), card.value)
+
+            if run_total + card.value <= HIGHEST_RUN_ALLOWED:
+                can_play_card = True
+
+        return can_play_card
+
+    def _play_run_card(self, active_run_player, active_run_hand):
+        run_total = CribbageGame.get_cards_total_value(self.run)
+        logging.info("Player #%i can play against run %i",
+          self.run_turn, run_total)
+
+        run_card = active_run_player.get_run_card(
+                     active_run_hand, self.run, run_total)
+
+        points_for_card = calculate_score_for_run_play(self.run, run_card)
+        logging.info("Player #%i played %s for %i points against the run %s",
+          self.run_turn, run_card.get_display(), points_for_card,
+          cards_as_string(self.run))
+
+        if self.run_turn == 1:
+            self.player_two_score += points_for_card
+            logging.info("Player #2 scores to total %i", self.player_two_score)
+        else:
+            self.player_one_score += points_for_card
+            logging.info("Player #1 scores to total %i", self.player_one_score)
+
+        active_run_hand.remove(run_card)
+        self.run.append(run_card)
+
+        logging.info("Player #%i played %s, the run is now %s",
+          self.run_turn, run_card.get_display(), cards_as_string(self.run))
+
+        return points_for_card
 
 class CribbageEngine:
     """
@@ -306,8 +352,7 @@ class CribbageEngine:
 
     Methods
     -------
-    new_game:
-        re-initializes the game and deck
+    new_game: re-initializes the game and deck
     """
     def __init__(self):
         init_deck = set()
@@ -331,11 +376,26 @@ class CribbageEngine:
 
 
     def new_game(self, player_one, player_two):
+        """Creates and returns a new game with the given players.
+
+        Args:
+            player_one: a CribbagePlayer AI for player one
+            player_two: a CribbagePlayer AI for player two
+        Returns:
+            (CribbageGame) a new instance of a game
+        """
         return CribbageGame(self._base_deck, player_one, player_two)
 
 
 ## Static Helper Methods
 def cards_as_string(cards):
+    """Converts a list of PlayingCards into a comma-separated string.
+
+    Args:
+        cards - the list of PlayingCards
+    Returns:
+        (String) a comma-separated list of cards
+    """
     card_display_list = []
     for card in cards:
         card_display_list.append(card.get_display())
@@ -350,7 +410,7 @@ def calculate_score_for_run_play(run, run_card):
         run_card - a single PlayingCard that represents the players next play
 
     Returns:
-    	(int) the score for the play
+        (int) the score for the play
 
     Raises:
         TODO: Invalid Input errors
@@ -361,7 +421,7 @@ def calculate_score_for_run_play(run, run_card):
     if run:
         # 2 points if you get 15 or 31 in the run
         if run_total + run_card.value == 15 or run_total + run_card.value == 31:
-          run_play_score += 2
+            run_play_score += 2
 
         # 2 points for every pair.  This is a combinatorial function
         total_pairs = 0
@@ -394,6 +454,15 @@ def calculate_score_for_run_play(run, run_card):
     return run_play_score
 
 def calculate_score_for_hand(player_hand, start_card):
+    """Calculates the score of points for the current hand.
+    Args:
+        player_hand - a list of PlayingCard that represents the player's hand
+        start_card - a single PlayingCard that represents the start card
+
+    Returns:
+        (int) the score for the hand
+    """
+
     hand_play_score = 0
     player_full_hand = player_hand.copy()
 
@@ -421,14 +490,14 @@ def calculate_score_for_hand(player_hand, start_card):
         for combo in combinations_set:
             cards_total_value = CribbageGame.get_cards_total_value(combo)
             if cards_total_value == 15:
-              hand_play_score += 2
+                hand_play_score += 2
 
     ## Find all pairs, they each are 2 points
     ## Triples, Quadruples can be ignored, they are just combinations of pairs
     combinations_set = list(combinations(player_full_hand, 2))
     for combo in combinations_set:
-      if combo[0].face == combo[1].face:
-        hand_play_score += 2
+        if combo[0].face == combo[1].face:
+            hand_play_score += 2
 
     ## Find the runs, but don't count sub-runs.  Runs are between 3-5 cards. If the
     ## larger run of 4-5 is scored, subruns should be ignored.
@@ -448,8 +517,8 @@ def _calculate_score_for_hand_his_nob(player_hand, start_card):
     """
     hand_play_score = 0
     for card in player_hand:
-      if card.face == Face.JACK and card.suit == start_card.suit:
-        hand_play_score += 1
+        if card.face == Face.JACK and card.suit == start_card.suit:
+            hand_play_score += 1
 
     return hand_play_score
 
